c5c88eb7507b79cf2b31af1dab063f8efc728a28
JeffNeal <jeffneal11@gmail.com>
2022-12-11 16:56:34 -0600

    added stretchy net

--- src/index.js
+++ src/index.js
@@ -4,18 +4,10 @@ let gameCanvas = document.getElementById("game"),
   mouseX = 0,
   mouseY = 0,
   direction = "",
+  mousetravel = 0,
   lastX = [0, ""];
-//     mousemovemethod = function (e) {
 
-//   if (e.pageX < oldx) {
-//       direction = "left"
-//   } else if (e.pageX > oldx) {
-//       direction = "right"
-//   }
-
-//   oldx = e.pageX;
-
-// }
+var prevEvent, currentEvent;
 
 onmousemove = function (e) {
   let boundaries = {
@@ -25,6 +17,7 @@ onmousemove = function (e) {
     bottom:
       gameCanvas.offsetTop + gameCanvas.offsetHeight - this.window.scrollY,
   };
+  currentEvent = e;
   mouseX = e.clientX;
   mouseY = e.clientY;
   //   console.log(boundaries.top, mouseX, boundaries.bottom);
@@ -46,7 +39,11 @@ onmousemove = function (e) {
     } else if (mouseX > lastX[0]) {
       direction = "right";
     }
-    // console.log(direction);
+
+    if (lastX[0] > -1) {
+      mousetravel += Math.max(Math.abs(mouseX - lastX[0]));
+    }
+    //console.log(mousetravel);
 
     if (lastX[1] !== direction) {
       console.log("changed!");
@@ -65,3 +62,47 @@ function flipNet() {
     netShaft.classList.add("flip");
   }
 }
+
+var maxSpeed = 0,
+  prevSpeed = 0,
+  maxPositiveAcc = 0,
+  maxNegativeAcc = 0;
+setInterval(function () {
+  if (prevEvent && currentEvent) {
+    var movementX = Math.abs(currentEvent.screenX - prevEvent.screenX);
+    var movementY = Math.abs(currentEvent.screenY - prevEvent.screenY);
+    var movement = Math.sqrt(movementX * movementX + movementY * movementY);
+
+    // document.getElementById("movementX").innerText=movementX;
+    // document.getElementById("movementY").innerText=movementY;
+    // document.getElementById("movement").innerText=Math.round(movement);
+
+    var speed = 10 * movement; //current speed
+
+    // document.getElementById("speed").innerText=Math.round(speed);
+    // document.getElementById("maxSpeed").innerText=Math.round(
+    //   speed>maxSpeed?(maxSpeed=speed):maxSpeed
+    // );
+
+    var acceleration = 10 * (speed - prevSpeed);
+
+    // document.getElementById("acceleration").innerText=Math.round(
+    //   acceleration
+    // );
+
+    if (acceleration > 0) {
+      // //   document.getElementById("maxPositiveAcceleration").innerText=Math.round(
+      //   acceleration>maxPositiveAcc?(maxPositiveAcc=acceleration):maxPositiveAcc
+      // );
+      netting.style.transform = `scale(${acceleration / 10000}, 1)`;
+      console.log(acceleration / 100000);
+    } else {
+      //   document.getElementById("maxNegativeAcceleration").innerText=Math.round(
+      //   acceleration<maxNegativeAcc?(maxNegativeAcc=acceleration):maxNegativeAcc
+      // );
+    }
+  }
+
+  prevEvent = currentEvent;
+  prevSpeed = speed;
+}, 100);
