48ffcc18f544ecb57d85365faf6bf242f9c6d24c
Jeffrey Neal <jeffneal11@gmail.com>
2022-12-14 09:07:49 -0600

    added net stretch on X axis

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 let gameCanvas = document.getElementById("game"),
   netShaft = document.getElementById("net-shaft"),
   netting = document.getElementById("netting"),
+  debug = document.getElementById("debugger"),
+  tracker = document.getElementById("tracker"),
   mouseX = 0,
   mouseY = 0,
   direction = "",
   mousetravel = 0,
-  lastX = [0, ""];
+  lastX = [0, ""],
+  lerp = 1,
+  acc = 0;
+let netFlip = 1;
 
 var prevEvent, currentEvent;
 
@@ -57,9 +62,11 @@ onmousemove = function (e) {
 
 function flipNet() {
   if (netShaft.classList.contains("flip")) {
+    netFlip = -1;
     netShaft.classList.remove("flip");
   } else {
     netShaft.classList.add("flip");
+    netFlip = 1;
   }
 }
 
@@ -77,25 +84,33 @@ setInterval(function () {
     // document.getElementById("movementY").innerText=movementY;
     // document.getElementById("movement").innerText=Math.round(movement);
 
-    var speed = 10 * movement; //current speed
+    var speed = movement; //current speed
 
     // document.getElementById("speed").innerText=Math.round(speed);
     // document.getElementById("maxSpeed").innerText=Math.round(
     //   speed>maxSpeed?(maxSpeed=speed):maxSpeed
     // );
 
-    var acceleration = 10 * (speed - prevSpeed);
+    var acceleration = speed - prevSpeed;
+    acc = acceleration;
+    // console.log(acc);
 
     // document.getElementById("acceleration").innerText=Math.round(
     //   acceleration
     // );
-
+    if (prevSpeed < speed) {
+      lerp += 1;
+    } else if (prevSpeed > speed) {
+      lerp -= 1;
+    }
+    //console.log(lerp);
+    //netting.style.transform = `scale(${lerp}, 1)`;
     if (acceleration > 0) {
       // //   document.getElementById("maxPositiveAcceleration").innerText=Math.round(
       //   acceleration>maxPositiveAcc?(maxPositiveAcc=acceleration):maxPositiveAcc
       // );
-      netting.style.transform = `scale(${acceleration / 10000}, 1)`;
-      console.log(acceleration / 100000);
+      //netting.style.transform = `scale(${lerp}, 1)`;
+      //console.log(acceleration / 100000);
     } else {
       //   document.getElementById("maxNegativeAcceleration").innerText=Math.round(
       //   acceleration<maxNegativeAcc?(maxNegativeAcc=acceleration):maxNegativeAcc
@@ -105,4 +120,73 @@ setInterval(function () {
 
   prevEvent = currentEvent;
   prevSpeed = speed;
-}, 100);
+}, 10);
+// setInterval(function () {
+//   if (acc > 0) {
+//     if (lerp < acc / 10000) {
+//       lerp += 0.1;
+//     } else if (lerp > acc / 10000) {
+//       lerp -= 0.1;
+//     }
+//   }
+//   netting.style.transform = `scale(${lerp}, 1)`;
+
+//   console.log(lerp.toFixed(2));
+// }, 10);
+let netXoffset = 200;
+setInterval(function () {
+  debug.style.left = `${netShaft.offsetLeft + 50 + netXoffset * netFlip}px`;
+  debug.style.top = `${netShaft.offsetTop + 2500}px`;
+  //netting.style.transform = `scale(${lerp}, 1)`;
+
+  // console.log(lerp.toFixed(2));
+}, 10);
+
+tracker.style.left = debug.style.left;
+tracker.style.top = debug.style.top;
+
+setInterval(function () {
+  if (tracker.offsetLeft - debug.offsetLeft > 100) {
+    tracker.style.left = `${debug.offsetLeft + 100}px`;
+  } else if (tracker.offsetLeft - debug.offsetLeft < -100) {
+    tracker.style.left = `${debug.offsetLeft - 100}px`;
+  }
+  if (tracker.offsetLeft < debug.offsetLeft) {
+    tracker.style.left = `${tracker.offsetLeft + 1}px`;
+  } else if (tracker.offsetLeft > debug.offsetLeft) {
+    tracker.style.left = `${tracker.offsetLeft - 1}px`;
+  }
+  if (tracker.offsetTop < debug.offsetTop) {
+    tracker.style.top = `${tracker.offsetTop + 1}px`;
+  } else if (tracker.offsetTop > debug.offsetTop) {
+    tracker.style.top = `${tracker.offsetTop - 1}px`;
+  }
+  console.log(tracker.offsetLeft - debug.offsetLeft);
+  // tracker.style.left = `${netShaft.offsetLeft + 50 + netXoffset * netFlip}px`;
+  // tracker.style.top = `${netShaft.offsetTop + 2500}px`;
+  //netting.style.transform = `scale(${lerp}, 1)`;
+
+  // console.log(lerp.toFixed(2));
+}, 1);
+tracker.style.left = debug.style.left;
+tracker.style.top = debug.style.top;
+let netDist = 0;
+let netDistY = 0;
+setInterval(function () {
+  netDist = ((debug.offsetLeft - tracker.offsetLeft) * netFlip * -1) / 100;
+  netDistY = (debug.offsetTop - tracker.offsetTop) / 100;
+  if (netDist > 1) {
+    netting.style.transform = `scale(${2}, 1)`;
+  } else if (netDist < -2) {
+    netting.style.transform = `scale(${-1}, 1)`;
+  } else {
+    netting.style.transform = `scale(${-netDist + 1}, 1)`;
+
+    netting.style.transform = `scale(${netDist + 1}, 1)`;
+  }
+  netting.style.transform += `skew(0, ${netDistY}deg)`;
+
+  //console.log(netDist);
+}, 10);
+
+//+ netXoffset * netFlip
